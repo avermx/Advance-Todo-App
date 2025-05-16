7bb208602c1de502790d8cc329f15942635e9ab8
avermx <aniket125verma@gmail.com>
2025-05-17 01:19:59 +0530

    created-signup-page

--- vite-project/src/components/Auth/Register.jsx
+++ vite-project/src/components/Auth/Register.jsx
@@ -1,24 +1,75 @@
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/input";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../ui/card";
+import { Button } from "../ui/button";
 
 const Register = () => {
     return (
-        <div className="h-screen  bg-[#09080A] flex justify-center items-center ">
-            <div className="bg-[#151515] w-[30%] h-[70%] rounded-xl  p-8 flex flex-col gap-5">
-                <div className=" flex flex-col gap-2">
-                    <h1 className="text-white  text-3xl text-center">Create an account</h1>
-                    <h2 className="text-xl text-gray-400 text-center">Sign up to start managing your tasks</h2>
-                </div>
-                <div className="w-full flex flex-col gap-2 text-white">
-                    <h1>Email</h1>
-                    <Input placeholder='name@example.com' />
-                </div>
-                <div className="">
-                    <h1>Password</h1>
-                    
-                </div>
-            </div>
-        </div>
-    )
-}
+        <div className=" h-[100vh] flex justify-center items-center dark text-3xl bg-black">
+            <Card className="h-[80%] w-[28%] ">
+                <CardHeader className="text-center">
+                    <CardTitle>Sign Up</CardTitle>
+                    <CardDescription className="text-[1rem]">
+                        Create an account to get started
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="flex flex-col gap-3 text-[1rem] ">
+                    <p>Name</p>
+                    <Input />
+                    <p>Email</p>
+                    <Input />
+                    <p>Password</p>
+                    <Input />
+                    <p>Confirm Password</p>
+                    <Input />
+                </CardContent>
+                <CardFooter className="text-center flex items-center justify-center flex-col gap-6">
+                    <Button>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="lucide lucide-user-plus mr-2 h-4 w-4"
+                            data-lov-id="src/pages/SignUp.tsx:150:12"
+                            data-lov-name="UserPlus"
+                            data-component-path="src/pages/SignUp.tsx"
+                            data-component-line="150"
+                            data-component-file="SignUp.tsx"
+                            data-component-name="UserPlus"
+                            data-component-content="%7B%22className%22%3A%22mr-2%20h-4%20w-4%22%7D"
+                        >
+                            <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path>
+                            <circle cx="9" cy="7" r="4"></circle>
+                            <line x1="19" x2="19" y1="8" y2="14"></line>
+                            <line x1="22" x2="16" y1="11" y2="11"></line>
 
-export default Register
+                        </svg>
+                        Create Account
+                    </Button>
+                    <CardContent className="text-[1rem] flex gap-1">
+                        <span>
+                            Already have an account? 
+                        </span>
+                        <span className="underline hover:cursor-pointer" >
+                              Sign in
+                        </span>
+                    </CardContent>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+};
+
+export default Register;
